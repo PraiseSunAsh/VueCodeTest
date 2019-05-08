a73a3f2330e071a5d864dc19b425da1e3b42b917
Jasonlee <690163223@qq.com>
2019-05-08 16:15:35 +0800

    append

---
+++ 06_v-model/js/main.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el:'#app',
+    //暂时理解为数据池
+    data:{
+       name:'whh',
+       number:'777',
+       sex:'female',
+       tendsex:['male'],
+       article:"bdsfbhdbfhjdb sfhjb   shfb",
+       from:3,
+       dest:[],
+    },
+   
+});
