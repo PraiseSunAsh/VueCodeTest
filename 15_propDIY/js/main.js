b244c8af11f973d5f572d74a5f9c042d3dee41c0
Jasonlee <690163223@qq.com>
2019-05-11 09:19:37 +0800

    append

---
+++ 15_propDIY/js/main.js
@@ -0,0 +1,26 @@
+//自定义属性
+Vue.directive('pin', function (el,binding) {
+    // console.log(binding.value);//第二个传的参数是自定义属性的value
+    if(binding.value){
+        el.style.position = 'fixed';
+        el.style.top = '10px';
+        el.style.left = '10px';
+    }else{
+        el.style.position = 'static';
+    }
+})
+
+
+new Vue({
+    el: '#app',
+    data:{
+        card1:{pinned:false},
+        card2:{pinned:false},
+    },
+    methods: {
+        onClick:function(){
+            //状态切换
+            this.card1.pinned =! this.card1.pinned;
+        }
+    },
+})
