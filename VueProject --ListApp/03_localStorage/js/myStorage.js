a3b981c13f875038d2cbe8d893cecbbf88bb9175
Jasonlee <690163223@qq.com>
2019-05-12 16:36:18 +0800

    project

---
+++ VueProject --ListApp/03_localStorage/js/myStorage.js
@@ -0,0 +1,23 @@
+(function(){
+    function set(key,val) {
+        //将JS对象（val）转换为JSON数据存起来
+        localStorage.setItem(key,JSON.stringify(val));
+    }
+
+    function get(key){
+        var json = localStorage.getItem(key);
+        //在取json数据前先判断下数据是否存在
+        if(json){
+            //如果没有的话就直接return undefined
+            return JSON.parse(json);
+        }
+    }
+
+    //别忘了把方法暴露出去
+    window.myData = {
+        set:set,
+        get:get,
+    };
+})();
+myData.set('name','whh');
+console.log(myData.get('name'));
