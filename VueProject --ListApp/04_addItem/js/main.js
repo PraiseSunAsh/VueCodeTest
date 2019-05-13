57e8d4669d0897c379a3d1b55cc7225135d2e3b1
Jasonlee <690163223@qq.com>
2019-05-13 18:27:38 +0800

    project

---
+++ VueProject --ListApp/04_addItem/js/main.js
@@ -0,0 +1,30 @@
+;(function () {
+    'use strict';
+
+    new Vue({
+        el:'#main',
+        data:{
+            list:[],
+            current:{
+                title:'',
+            }
+        },
+        methods: {
+            add:function(){
+                // 如果不存在就不添加
+                if(!this.current.title) return;
+                // 添加之前需要浅拷贝一个指针
+                var todo = Object.assign({},this.current);
+                //然后把current推进去
+                this.list.push(todo);
+            },
+            update:function(){
+
+            },
+            remove:function(){
+            
+            },
+        },
+
+    });
+})();
